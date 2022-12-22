a4cb7baa0b8fdc40f9ea458d6deef61ec2485c9e
Matthew Wire <mjw@mjwconsult.co.uk>
2022-12-22 15:10:39 +0000

    Fixes to show/hide fields

--- js/contributionpage1.js
+++ js/contributionpage1.js
@@ -2,64 +2,62 @@
 
   window.addEventListener('DOMContentLoaded', function() {
 
+    var ukUniversitySelect = $('select#custom_25');
+    var professionalSectorSelect = $('select#custom_27');
+    var instituteField = $('div#editrow-custom_6');
+    var instituteFieldHelp = $('div#helprow-custom_6');
+    var ukUniversityField = $('div#editrow-custom_25');
+    var ukUniversityFieldHelp = $('div#helprow-custom_25');
+    var professionalSectorField = $('div#editrow-custom_27');
+    var professionalSectorFieldHelp = $('div#helprow-custom_27');
+    var professionalSectorOtherField = $('div#editrow-custom_28');
+
     // status radio selector
     $('input[name="custom_2"]').on('change', function() {
       statusShowHideFields();
     });
 
     // uk university drop-down
-    $('select#custom_25').on('change', function() {
+    ukUniversitySelect.on('change', function() {
       ukUniversityShowHideFields();
     });
     // professional sector drop-down
-    $('select#custom_27').on('change', function() {
+    professionalSectorSelect.on('change', function() {
       professionalSectorShowHideFields();
     });
 
     function statusShowHideFields() {
       var checkedInputID = $('input[name="custom_2"]:checked').attr('id');
-      var instituteField = $('div#editrow-custom_6');
-      var instituteFieldHelp = $('div#helprow-custom_6');
-      var ukUniversityField = $('div#editrow-custom_25');
-      var professionalSectorField = $('div#editrow-custom_27');
       switch (checkedInputID) {
         case 'CIVICRM_QFID_Student_custom_2':
         case 'CIVICRM_QFID_Academic_custom_2':
           // show uk university drop-down
           ukUniversityField.show();
-          professionalSectorField.hide();
+          ukUniversityFieldHelp.show();
+          hideProfessionalSectorField();
           break;
 
         case 'CIVICRM_QFID_Professional_custom_2':
         case 'CIVICRM_QFID_Retired_Professional_custom_2':
           // show the professional sector drop-down
           professionalSectorField.show();
+          professionalSectorFieldHelp.show();
+          hideUkUniversityField();
           break;
 
         case 'CIVICRM_QFID_Retired_Academic_custom_2':
           // show nothing
-          ukUniversityField.hide();
-          instituteField.hide();
-          instituteFieldHelp.hide();
-          professionalSectorField.hide();
-          break;
-
         case 'CIVICRM_QFID_Other_custom_2':
           // show nothing
         default:
           // show nothing
-          ukUniversityField.hide();
-          instituteField.hide();
-          instituteFieldHelp.hide();
-          professionalSectorField.hide();
+          hideUkUniversityField();
+          hideProfessionalSectorField();
       }
     }
 
     function ukUniversityShowHideFields() {
-      var ukUniversityField = $('div#editrow-custom_25');
-      var instituteField = $('div#editrow-custom_6');
-      var instituteFieldHelp = $('div#helprow-custom_6');
-      var selectedVal = $('select#custom_25').find(':selected').val();
+      var selectedVal = ukUniversitySelect.find(':selected').val();
 
       switch (selectedVal) {
         case 'Other':
@@ -76,16 +74,30 @@
     }
 
     function professionalSectorShowHideFields() {
-      var sectorOtherField = $('div#editrow-custom_28');
-      var selectedVal = $('select#custom_27').find(':selected').val();
+      var selectedVal = professionalSectorSelect.find(':selected').val();
       switch (selectedVal) {
         case 'Other Professional Sector':
-          sectorOtherField.show();
+          professionalSectorOtherField.show();
           break;
 
         default:
-          sectorOtherField.hide();
+          professionalSectorOtherField.hide();
       }
+    }
+
+    function hideUkUniversityField() {
+      ukUniversitySelect.select2().val('').trigger('change');
+      ukUniversityField.hide();
+      ukUniversityFieldHelp.hide();
+      instituteField.val('').hide();
+      instituteFieldHelp.hide();
+    }
+
+    function hideProfessionalSectorField() {
+      professionalSectorSelect.select2().val('').trigger('change');
+      professionalSectorField.hide();
+      professionalSectorFieldHelp.hide();
+      professionalSectorOtherField.val('').hide();
     }
 
     statusShowHideFields();
